d7d91d2d329a413f0467c94724a6f850dd844e33
Leeyoungsup <liive29252@gmail.com>
2026-01-08 11:53:22 +0900

    WSIViewer 별칭 업데이트: WSIViewWidget을 사용하여 호환성 유지 및 __all__ 목록 수정

--- ui/__init__.py
+++ ui/__init__.py
@@ -3,7 +3,11 @@ UI 모듈
 뷰어 및 UI 컴포넌트
 """
 
-from .viewer import WSIViewer, PathologyViewer
+from .viewer import PathologyViewer
+from .wsi_view_widget import WSIViewWidget
 from .minimap import MiniMap
 
-__all__ = ['WSIViewer', 'PathologyViewer', 'MiniMap']
+# Backward compatibility alias
+WSIViewer = WSIViewWidget
+
+__all__ = ['PathologyViewer', 'WSIViewWidget', 'WSIViewer', 'MiniMap']
