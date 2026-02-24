9ca5fc30245945ffe52aed27f274cf1de2924457
Leeyoungsup <liive29252@gmail.com>
2026-02-24 12:58:21 +0900

    feat: DetectionVisualizationDialog 및 관련 함수 추가

--- ui/dialogs/__init__.py
+++ ui/dialogs/__init__.py
@@ -3,5 +3,7 @@ UI 다이얼로그 모듈
 """
 
 from .slide_info_dialog import SlideInfoDialog, show_slide_info_dialog
+from .detection_visualization_dialog import DetectionVisualizationDialog, show_detection_visualization
 
-__all__ = ['SlideInfoDialog', 'show_slide_info_dialog']
+__all__ = ['SlideInfoDialog', 'show_slide_info_dialog',
+           'DetectionVisualizationDialog', 'show_detection_visualization']
